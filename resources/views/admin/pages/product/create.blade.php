6c106dca62ae3c5380c4a0c9af0d20893ea1fd11
huychongxang <huychongxang@gmail.com>
2020-04-01 17:59:54 +0700

    tích hợp tinymce editor

--- resources/views/admin/pages/product/create.blade.php
+++ resources/views/admin/pages/product/create.blade.php
@@ -131,6 +131,7 @@
     <!-- /.row -->
 @endsection
 @push('scripts')
+    <script src="{{asset('admin/tinymce/js/tinymce/tinymce.min.js')}}"></script>
     <script>
         $('.select2').select2({
             theme: "classic",
@@ -139,5 +140,61 @@
             autoclose: true,
             format: 'yyyy-mm-dd'
         })
+        tinymce.init({
+
+            editor_selector : "mceEditor",
+
+            selector: '#mytextarea',
+
+            plugins: [
+
+                'advlist autolink lists link image charmap print preview hr anchor pagebreak',
+
+                'searchreplace wordcount visualblocks visualchars code fullscreen',
+
+                'insertdatetime media nonbreaking save table contextmenu directionality',
+
+                'emoticons template paste textcolor colorpicker textpattern imagetools responsivefilemanager'
+
+            ],
+
+            toolbar1: 'insertfile undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image | responsivefilemanager',
+
+            toolbar2: 'print preview media | forecolor backcolor emoticons',
+
+            image_advtab: true,
+
+            templates: [
+
+                { title: 'Test template 1', content: 'Test 1' },
+
+                { title: 'Test template 2', content: 'Test 2' }
+
+            ],
+            image_title: true,
+            automatic_uploads: true,
+            file_picker_types: 'image',
+            file_picker_callback: function(cb, value, meta) {
+                var input = document.createElement('input');
+                input.setAttribute('type', 'file');
+                input.setAttribute('accept', 'image/*');
+                input.onchange = function () {
+                    var file = this.files[0];
+                    var reader = new FileReader();
+
+                    reader.onload = function () {
+                        var id = 'blobid' + (new Date()).getTime();
+                        var blobCache = tinymce.activeEditor.editorUpload.blobCache;
+                        var base64 = reader.result.split(',')[1];
+                        var blobInfo = blobCache.create(id, file, base64);
+                        blobCache.add(blobInfo);
+                        cb(blobInfo.blobUri(), {title: file.name});
+                    };
+                    reader.readAsDataURL(file);
+                };
+                input.click();
+            }
+
+        });
     </script>
 @endpush
