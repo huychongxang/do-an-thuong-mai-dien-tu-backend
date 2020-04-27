13f3edd3f1b0636aff1964a926cfddfe3d3b8826
huychongxang <huychongxang@gmail.com>
2020-04-27 14:15:43 +0700

    Biểu đồ 1

--- app/Http/Controllers/Cms/DashboardController.php
+++ app/Http/Controllers/Cms/DashboardController.php
@@ -8,6 +8,7 @@ use App\Models\Post;
 use App\Models\Product;
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class DashboardController extends Controller
 {
@@ -17,6 +18,50 @@ class DashboardController extends Controller
         $totalProducts = Product::count();
         $totalUsers = User::count();
         $totalPosts = Post::count();
-        return view('admin.pages.dashboard.index', compact('totalOrders', 'totalProducts', 'totalPosts', 'totalUsers'));
+
+        $totals = Order::select(
+            DB::raw(
+                'DATE(created_at) as date,
+                DATE_FORMAT(created_at, "%m/%d") as md,
+                sum(total) as total_amount,
+                count(id) as total_order'
+            )
+        )
+            ->groupBy('date', 'md')
+            ->having('date', '<=', date('Y-m-d'))
+            ->whereRaw('DATE(created_at) >=  DATE_SUB(DATE(NOW()), INTERVAL 1 MONTH)')
+            ->get();
+
+        $orderGroup = $totals->keyBy('md')->toArray();
+        $arrDays = [];
+        $arrTotalsOrder = [];
+        $arrTotalsAmount = [];
+        $rangDays = new \DatePeriod(
+            new \DateTime('-1 month'),
+            new \DateInterval('P1D'),
+            new \DateTime('+1 day')
+        );
+
+        foreach ($rangDays as $i => $day) {
+            $date = $day->format('m/d');
+            $arrDays[$i] = $date;
+            $arrTotalsAmount[$i] = isset($orderGroup[$date]) ? $orderGroup[$date]['total_amount'] : 0;
+            $arrTotalsOrder[$i] = isset($orderGroup[$date]) ? $orderGroup[$date]['total_order'] : 0;
+        }
+
+        $max_order = max($arrTotalsOrder);
+        foreach ($arrTotalsAmount as $key => $value) {
+            if ($key != 0) {
+                $key_first = $key - 1;
+                $arrTotalsAmount[$key] += $arrTotalsAmount[$key_first];
+            }
+        }
+        $arrDays = '["' . implode('","', $arrDays) . '"]';
+        $arrTotalsAmount = '[' . implode(',', $arrTotalsAmount) . ']';
+        $arrTotalsOrder = '[' . implode(',', $arrTotalsOrder) . ']';
+
+        return view('admin.pages.dashboard.index', compact('totalOrders', 'totalProducts', 'totalPosts', 'totalUsers',
+            'arrDays', 'arrTotalsAmount', 'arrTotalsOrder', 'max_order'
+        ));
     }
 }
--- resources/views/admin/pages/dashboard/index.blade.php
+++ resources/views/admin/pages/dashboard/index.blade.php
@@ -165,8 +165,120 @@
     <script>
         $(document).ready(function ($) {
             var ctx_month = document.getElementById('chart-days-in-month').getContext('2d');
+            new Chart(ctx_month, {
+                // The type of chart we want to create
+                type: 'bar',
 
+                // The data for our dataset
+                data: {
+                    // type: 'category',
+                    labels: {!! $arrDays !!},
+                    datasets: [
+                        {
+                            label: "Tổng giá trị",
+                            backgroundColor: 'rgba(225,0,0,0.4)',
+                            borderColor: "rgb(231, 53, 253)",
+                            borderCapStyle: 'square',
+                            pointHoverRadius: 8,
+                            pointHoverBackgroundColor: "yellow",
+                            pointHoverBorderColor: "brown",
+                            data: {!! $arrTotalsAmount !!},
+                            showLine: true, // disable for a single dataset,
+                            yAxisID: "y-axis-gravity",
+                            fill: false,
+                            type: 'line',
+                            lineTension: 0.1,
+                        },
+                        {
+                            label: "Tổng đơn hàng",
+                            backgroundColor: 'rgb(138, 199, 214)',
+                            borderColor: 'rgb(138, 199, 214)',
+                            pointHoverRadius: 8,
+                            pointHoverBackgroundColor: "brown",
+                            pointHoverBorderColor: "yellow",
+                            data: {!! $arrTotalsOrder !!},
+                            showLine: true, // disable for a single dataset,
+                            yAxisID: "y-axis-density",
+                            spanGaps: true,
+                            lineTension: 0.1,
+
+                        },
+
+                    ]
+                },
+
+                // Configuration options go here
+                options: {
+                    responsive: true,
+                    legend: {
+                        display: true,
+                    },
+                    layout: {
+                        padding: {
+                            left: 10,
+                            right: 10,
+                            top: 0,
+                            bottom: 0
+                        }
+                    },
+                    scales: {
+                        yAxes: [
+                            {
+                                position: "left",
+                                id: "y-axis-density",
+                                ticks: {
+                                    beginAtZero: true,
+                                    max: {!! $max_order  !!} +5,
+                                    min: 0,
+                                    stepSize: 1,
+                                },
+                                scaleLabel: {
+                                    display: true,
+                                    labelString: 'Tổng đơn hàng',
+                                    fontSize: 15,
+
+                                }
+                            },
+                            {
+                                position: "right",
+                                id: "y-axis-gravity",
+                                ticks: {
+                                    beginAtZero: true,
+                                    callback: function (label, index, labels) {
+                                        return format_number(label);
+                                    },
+                                },
+                                scaleLabel: {
+                                    display: true,
+                                    labelString: 'Tổng giá trị',
+                                    fontSize: 15
+                                }
+                            }
+                        ]
+                    },
+
+                    tooltips: {
+                        callbacks: {
+                            label: function (tooltipItem, data) {
+                                var label = data.datasets[tooltipItem.datasetIndex].label || '';
+
+                                if (label) {
+                                    label += ': ';
+                                }
+                                label += format_number(tooltipItem.yLabel);
+                                return label;
+                            }
+                        }
+                    }
+                }
+            });
         });
+
+        function format_number(n) {
+            return n.toFixed(0).replace(/./g, function (c, i, a) {
+                return i > 0 && c !== "." && (a.length - i) % 3 === 0 ? "," + c : c;
+            });
+        }
     </script>
 
 @endpush
