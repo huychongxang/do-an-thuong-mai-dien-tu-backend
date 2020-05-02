072e1419dbe7786fe23e5f00a5fd7a806e1566eb
huychongxang <huychongxang@gmail.com>
2020-05-02 17:03:23 +0700

    ghép trang home

---
+++ resources/views/web/includes/filter-all-fashion-1.blade.php
@@ -0,0 +1,156 @@
+<section id="all-fashion-1" class="space-35">
+    <div class="container theme-container">
+        <div class="title-wrap with-border">
+            <h2 class="section-title with-border">
+                            <span class="white-bg">
+                                <span class="funky-font blue-tag">Child</span>
+                                <span class="italic-font">Fashion (0-5 Years)</span>
+                            </span>
+            </h2>
+            <h3 class="sub-title">Fashion: All for the small</h3>
+            <hr class="dash-divider">
+        </div>
+        <div class="row">
+            <div class="col-md-3 col-sm-6 fashion-bg">
+                <div class="fashion-wrap height-445">
+                    <div class="filter-wrap">
+                        <h2 class="filter-title blue-bg-with-shadow">Filter Option</h2>
+                        <form class="filter-form">
+                            <div class="form-group selectpicker-wrapper">
+                                <select
+                                        class="selectpicker input-price" data-live-search="true" data-width="100%"
+                                        data-toggle="tooltip" title="Looking to Buy">
+                                    <option>Option 1</option>
+                                    <option>Option 2</option>
+                                    <option>Option 3</option>
+                                </select>
+                            </div>
+                            <div class="form-group selectpicker-wrapper">
+                                <select
+                                        class="selectpicker input-price" data-live-search="true" data-width="100%"
+                                        data-toggle="tooltip" title="Child Age">
+                                    <option>Option 1</option>
+                                    <option>Option 2</option>
+                                    <option>Option 3</option>
+                                </select>
+                            </div>
+                            <div class="form-group selectpicker-wrapper">
+                                <select
+                                        class="selectpicker input-price" data-live-search="true" data-width="100%"
+                                        data-toggle="tooltip" title="Category">
+                                    <option>Option 1</option>
+                                    <option>Option 2</option>
+                                    <option>Option 3</option>
+                                </select>
+                            </div>
+                            <div class="form-group">
+                                <button type="submit" class="blue-btn btn">Go</button>
+                            </div>
+                        </form>
+                        <div class="filter-img">
+                            <img src="{{asset('web/assets/img/fashion/filter-1.png')}}" alt=" ">
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-3 col-sm-6 fashion-bg">
+                <div class="fashion-wrap light-bg">
+                    <div class="box-container height-445">
+                        <div class="box-img-wrap img-right">
+                            <img alt=" " src="{{asset('web/assets/img/fashion/fashion-1.png')}}">
+                        </div>
+
+                        <div class="col-md-6 text-center pull-left">
+                            <div class="fashion-new">
+                                <div class="pink-new-tag new-tag">
+                                    <a href="#" class="funky-font">New</a>
+                                </div>
+                            </div>
+                            <div class="title-wrap">
+                                <h2 class="section-title with-divider">
+                                                <span>
+                                                    <span class="funky-font pink-tag">Child </span>
+                                                    <span class="italic-font">Accessories & Wear</span>
+                                                </span>
+                                </h2>
+                                <h3 class="sub-title-small">Upto <span class="bold-font pink-color"> 80% </span> Off
+                                </h3>
+                                <a href="#" class="pink-color title-link space-top-35"> Shop Now <i
+                                            class="fa fa-caret-right"></i> </a>
+                            </div>
+                        </div>
+                        <div class="fashion-icon">
+                            <img src="{{asset('web/assets/img/fashion/icon-1.png')}}" alt=" ">
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-6 col-sm-12 fashion-bg">
+                <div class="fashion-wrap light-bg">
+                    <div class="box-container height-195">
+                        <div class="box-img-wrap img-right">
+                            <img alt=" " src="{{asset('web/assets/img/fashion/fashion-2.png')}}">
+                        </div>
+                        <div class="col-md-6 col-sm-5 pull-left">
+                            <div class="title-wrap">
+                                <h2 class="section-title">
+                                                <span>
+                                                    <span class="funky-font blue-tag">Shoes </span>
+                                                    <span class="italic-font">For Childrens</span>
+                                                </span>
+                                </h2>
+                                <h3 class="sub-title-small font-20">Stylish Footwares</h3>
+                                <hr class="dash-divider">
+                                <a href="#" class="blue-btn btn"> Shop Now!<i class="fa fa-caret-right"></i></a>
+                            </div>
+                        </div>
+                        <div class="fashion-icon">
+                            <img src="{{asset('web/assets/img/fashion/icon-2.png')}}" alt=" ">
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-3 col-sm-6 fashion-bg">
+                <div class="fashion-wrap light-bg">
+                    <div class="box-container height-195">
+                        <div class="box-img-wrap img-right">
+                            <img alt=" " src="{{asset('web/assets/img/fashion/fashion-3.png')}}">
+                        </div>
+                        <div class="first-smile">
+                            <a href="#" class="section-title">
+                                            <span>
+                                                <span class="funky-font green-tag">First </span>
+                                                <span class="italic-font">Smile</span>
+                                            </span>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-3 col-sm-6 fashion-bg">
+                <div class="fashion-wrap height-205 light-bg">
+                    <div class="box-container height-195">
+                        <div class="box-img-wrap img-right">
+                            <img alt=" " src="{{asset('web/assets/img/fashion/fashion-4.png')}}">
+                        </div>
+                        <div class="col-md-6 col-sm-6  text-center pull-left">
+                            <div class="title-wrap">
+                                <h2 class="section-title with-divider">
+                                                <span>
+                                                    <span class="sub-title-small font-20">Clothes </span>
+                                                    <span class="italic-font">For Baby</span>
+                                                </span>
+                                </h2>
+                                <a href="#" class="pink-color title-link"> Shop Now <i
+                                            class="fa fa-caret-right"></i> </a>
+                            </div>
+                        </div>
+                        <div class="fashion-icon">
+                            <img src="{{asset('web/assets/img/fashion/icon-3.png')}}" alt=" ">
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
---
+++ resources/views/web/includes/filter-all-fashion-2.blade.php
@@ -0,0 +1,137 @@
+<section id="all-fashion-2" class="space-35">
+    <div class="container theme-container">
+        <div class="title-wrap with-border">
+            <h2 class="section-title with-border">
+                            <span class="white-bg">
+                                <span class="funky-font blue-tag">Kids</span>
+                                <span class="italic-font">Fashion (6-12 Years)</span>
+                            </span>
+            </h2>
+            <h3 class="sub-title">Turn your Li'l Boy into a Rockstar</h3>
+            <hr class="dash-divider">
+        </div>
+        <div class="row">
+            <div class="col-md-5 col-sm-12">
+                <div class="row">
+                    <div class="col-md-12 col-sm-6 fashion-bg-2">
+                        <div class="fashion-wrap light-bg">
+                            <div class="box-container height-250">
+                                <div class="box-img-wrap img-right">
+                                    <img src="{{asset('web/assets/img/fashion/fashion-5.png')}}" alt=" ">
+                                </div>
+                                <div class="col-md-7 pull-left">
+                                    <div class="title-wrap">
+                                        <h2 class="section-title">
+                                                        <span>
+                                                            <span class="funky-font pink-tag">Gift </span>
+                                                            <span class="italic-font">Voucher</span>
+                                                        </span>
+                                        </h2>
+                                        <h3 class="sub-title-small">Free Gift offers</h3>
+                                        <hr class="dash-divider-small">
+                                        <h3 class="sub-title-small">More Then <span
+                                                    class="bold-font-block pink-color"> $200 </span> Shopping</h3>
+                                    </div>
+                                </div>
+                                <div class="fashion-icon">
+                                    <img src="{{asset('web/assets/img/fashion/icon-4.png')}}" alt=" ">
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-12 col-sm-6 fashion-bg-2">
+                        <div class="fashion-wrap light-bg">
+                            <div class="box-container">
+                                <div class="box-img-wrap">
+                                    <img src="{{asset('web/assets/img/fashion/fashion-6.png')}}" alt=" ">
+                                </div>
+                                <div class="col-md-7 pull-right">
+                                    <div class="title-wrap">
+                                        <h2 class="section-title">
+                                                        <span>
+                                                            <span class="funky-font blue-tag">Girls </span>
+                                                            <span class="italic-font">Accessories</span>
+                                                        </span>
+                                        </h2>
+                                        <h3 class="sub-title-small">Stylish Sunglasses</h3>
+                                        <hr class="dash-divider">
+                                        <a class="blue-color title-link" href="#"> Shop Now <i
+                                                    class="fa fa-caret-right"></i> </a>
+                                    </div>
+                                </div>
+                                <div class="fashion-icon-right">
+                                    <img src="{{asset('web/assets/img/fashion/icon-5.png')}}" alt=" ">
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-3 col-sm-6">
+                <div class="fashion-wrap">
+                    <div class="filter-wrap height-445">
+                        <h2 class="filter-title green-bg-with-shadow">Filter Option</h2>
+                        <form class="filter-form">
+                            <div class="form-group selectpicker-wrapper">
+                                <select
+                                        class="selectpicker input-price" data-live-search="true" data-width="100%"
+                                        data-toggle="tooltip" title="Looking to Buy">
+                                    <option>Option 1</option>
+                                    <option>Option 2</option>
+                                    <option>Option 3</option>
+                                </select>
+                            </div>
+                            <div class="form-group selectpicker-wrapper">
+                                <select
+                                        class="selectpicker input-price" data-live-search="true" data-width="100%"
+                                        data-toggle="tooltip" title="Child Age">
+                                    <option>Option 1</option>
+                                    <option>Option 2</option>
+                                    <option>Option 3</option>
+                                </select>
+                            </div>
+                            <div class="form-group selectpicker-wrapper">
+                                <select
+                                        class="selectpicker input-price" data-live-search="true" data-width="100%"
+                                        data-toggle="tooltip" title="Category">
+                                    <option>Option 1</option>
+                                    <option>Option 2</option>
+                                    <option>Option 3</option>
+                                </select>
+                            </div>
+                            <div class="form-group">
+                                <button type="submit" class="green-btn btn">Go</button>
+                            </div>
+                        </form>
+                        <div class="filter-img">
+                            <img src="{{asset('web/assets/img/fashion/filter-2.png')}}" alt=" ">
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4 col-sm-6">
+                <div class="circle-box light-bg">
+                    <div class="text-center">
+                        <div class="title-wrap">
+                            <h2 class="section-title">
+                                            <span>
+                                                <span class="funky-font blue-tag">Future </span>
+                                                <span class="italic-font">Your kids on</span>
+                                            </span>
+                            </h2>
+                            <a href="index-2.html" class="logo"><img src="{{asset('web/assets/img/logo/logo.png')}}" alt=" "></a>
+                        </div>
+                        <div class="text-box">
+                            <p>Suspendisse lobortis vesti eros sit amet ullamcorper. Donec mollis..!!</p>
+                            <a href="#" class="blue-color title-link"> Read More <i class="fa fa-caret-right"></i>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+                <div class="banner-img">
+                    <img src="{{asset('web/assets/img/fashion/banner-1.png')}}" alt=" ">
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
