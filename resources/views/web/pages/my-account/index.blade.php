d7d468a05ae8cd98c4cfeec73d2935aa3f53508d
Truong Khanh Huy <huychongxang@gmail.com>
2020-05-27 15:07:29 +0700

    viet hoa

--- resources/views/web/pages/my-account/index.blade.php
+++ resources/views/web/pages/my-account/index.blade.php
@@ -13,18 +13,15 @@
             <!-- Sidebar Start -->
             <aside class="col-md-4 col-sm-4 space-bottom-20">
                 <div class="blog-sidebar-widget light-bg default-box-shadow">
-                    <h4 class="widget-title blue-bg"> <span>  Account  </span> </h4>
+                    <h4 class="widget-title blue-bg"> <span>  Tài khoản  </span> </h4>
                     <div class="blog-widget-content">
                         <ul>
-                            <li  class="accout-item"><a href="account-info.html"> Account Information </a></li>
-                            <li  class="accout-item active"><a href="my-account.html">My Account</a></li>
-                            <li  class="accout-item"><a href="cng-pw.html">Change Password</a></li>
-                            <li  class="accout-item"><a href="address-book.html">Address Books</a></li>
-                            <li  class="accout-item"><a href="order-history.html">Order History</a></li>
-                            <li  class="accout-item"><a href="review-rating.html">Reviews and Ratings</a></li>
-                            <li  class="accout-item"><a href="return.html">Returns Requests</a></li>
-                            <li  class="accout-item"><a href="newsletter.html">Newsletter</a></li>
-                            <li  class="accout-item"><a href="myaccount-leftsidebar.html">Left Sidebar</a></li>
+                            <li  class="accout-item"><a href="account-info.html"> Thông tin tài khoản </a></li>
+                            <li  class="accout-item active"><a href="my-account.html">Tài khoản của tôi</a></li>
+                            <li  class="accout-item"><a href="cng-pw.html">Đổi mật khẩu</a></li>
+                            <li  class="accout-item"><a href="address-book.html">Địa chỉ</a></li>
+                            <li  class="accout-item"><a href="order-history.html">Lịch sử đơn hàng</a></li>
+                            <li  class="accout-item"><a href="return.html">Trạng thái hoàn trả</a></li>
                         </ul>
                     </div>
                 </div>
@@ -42,44 +39,33 @@
                     </h2>
                 </div>
                 <div class="account-details-wrap">
-                    <div class="title-2 sub-title-small">  My Account</div>
+                    <div class="title-2 sub-title-small">  Tài khoản của tôi</div>
                     <div class="account-box  light-bg default-box-shadow">
                         <ul>
                             <li>
-                                <a href="account-info.html">Edit your account information</a>
+                                <a href="account-info.html">Sửa thông tin tài khoản</a>
                             </li>
                             <li>
-                                <a href="cng-pw.html">Change your password</a>
+                                <a href="cng-pw.html">Đổi mật khẩu</a>
                             </li>
                             <li>
-                                <a href="address-book.html">Modify your address book entries</a>
+                                <a href="address-book.html">Đổi địa chỉ</a>
                             </li>
                         </ul>
                     </div>
 
-                    <div class="title-2 sub-title-small"> order and review </div>
+                    <div class="title-2 sub-title-small"> Đơn hàng </div>
                     <div class="account-box  light-bg default-box-shadow">
                         <ul>
                             <li>
-                                <a href="order-history.html">View your order history</a>
+                                <a href="order-history.html">Xem lịch sử đơn hàng</a>
                             </li>
                             <li>
-                                <a href="review-rating.html">Your reviews and ratings</a>
-                            </li>
-                            <li>
-                                <a href="return.html">View your retun requests</a>
+                                <a href="return.html">Xem trạng thái hoàn trả</a>
                             </li>
                         </ul>
                     </div>
-
-                    <div class="title-2 sub-title-small">  Newsletter </div>
-                    <div class="account-box  light-bg default-box-shadow">
-                        <ul>
-                            <li>
-                                <a href="newsletter.html">Subscribe / unsubscribe to newsletter</a>
-                            </li>
-                        </ul>
-                    </div>
+                    
                 </div>
             </aside>
             <!-- Posts Ends -->
