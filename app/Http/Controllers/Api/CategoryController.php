28db953ac9f1b8781aa3af63687baecc673d944f
Truong Khanh Huy <huychongxang@gmail.com>
2020-06-05 15:17:29 +0700

    fix api get list product category

--- app/Http/Controllers/Api/CategoryController.php
+++ app/Http/Controllers/Api/CategoryController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers\Api;
 use App\Helpers\ApiHelper;
 use App\Http\Controllers\Controller;
 use App\Http\Resources\Api\Category\CategoriesResource;
-use App\Models\Category;
+use App\Models\ProductCategory;
 use Illuminate\Http\Request;
 
 class CategoryController extends Controller
@@ -15,7 +15,7 @@ class CategoryController extends Controller
     public function index()
     {
         try {
-            $categories = Category::paginate($this->limit);
+            $categories = ProductCategory::paginate($this->limit);
             $resource = CategoriesResource::collection($categories);
             return ApiHelper::api_resource_handle($resource, 200, [
                 'success' => true
