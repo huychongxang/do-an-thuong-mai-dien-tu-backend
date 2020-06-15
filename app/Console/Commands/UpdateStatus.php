072bc15a06e799911ec9e197ccadada03126f26f
Truong Khanh Huy <huychongxang@gmail.com>
2020-06-15 11:03:04 +0700

    test cronjob

---
+++ app/Console/Commands/UpdateStatus.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Faker\Factory;
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\DB;
+
+class UpdateStatus extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'status:update';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        $faker = Factory::create();
+        DB::table('test')->insert([
+            'content' => $faker->name
+        ]);
+    }
+}
